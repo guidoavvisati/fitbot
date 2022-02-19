76cf680703171d0278cc866951187a6a7b1303c1
Guido Avvisati <guido.avvisati@gmail.com>
2022-02-19 17:49:18 +0100

    added procfile and handling credentials as env vars

--- clock.py
+++ clock.py
@@ -9,8 +9,4 @@ def timed_job():
     reply_to_tweet()
 
 
-# @sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
-# def scheduled_job():
-#     print('This job is run every weekday at 5pm.')
-
 sched.start()
